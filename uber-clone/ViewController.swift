7c0aeb151e4e341377c28ae0131e54dafd4bd72c
Michael Luo <michael.luo0801@gmail.com>
2019-04-24 22:00:42 +1000

    first commit

---
+++ uber-clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uber-clone
+//
+//  Created by Michael Luo on 24/4/19.
+//  Copyright © 2019 Michael Luo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
