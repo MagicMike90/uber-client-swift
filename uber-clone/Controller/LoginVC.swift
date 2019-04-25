15cf4bedae73c8a7687606d295a8f0f040fe2703
Michael Luo <michael.luo0801@gmail.com>
2019-04-26 00:09:39 +1000

    leeave keyboard bug

--- uber-clone/Controller/LoginVC.swift
+++ uber-clone/Controller/LoginVC.swift
@@ -9,15 +9,32 @@
 import UIKit
 
 class LoginVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        view.bindToKeyboard()
+        
+        let tapGesture = UITapGestureRecognizer(target: self.view, action: #selector(self.handleScreenTap(send:)))
+        tapGesture.numberOfTapsRequired = 1
+        tapGesture.numberOfTouchesRequired = 1
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    override func touchesBegan(_ touches: Set<UITouch>,
+                               with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     @IBAction func cancelBtnPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+}
+extension UIViewController {
+    
+    @objc func handleScreenTap(send: UITapGestureRecognizer) -> Void {
+        print("handleScreenTap")
+        self.view.endEditing(true)
+    }
 }
--- uber-clone/Extensions/UIViewExt.swift
+++ uber-clone/Extensions/UIViewExt.swift
@@ -14,4 +14,26 @@ extension UIView {
             self.alpha = alphaValue
         }
     }
+    
+    func bindToKeyboard() {
+//         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+
+ 
+    @objc func keyboardWillChange(_ notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let duration =  userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+            let curve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+            let curFrame = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+            let targetFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+            let deltaY = targetFrame.origin.y - curFrame.origin.y
+
+            print("deltaY \(deltaY)")
+            UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIView.KeyframeAnimationOptions(rawValue: curve), animations: {
+                self.frame.origin.y += deltaY
+            }, completion: nil)
+        }
+    }
+    
 }
